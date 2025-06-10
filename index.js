765556f5409b6e446c0207d48c055cadd98d50d0
vishrutimahajan <140059070+vishrutimahajan@users.noreply.github.com>
2025-06-10 14:23:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Counter from './counter';
+import App from './App'; // <-- Make sure App.js uses `export default App`
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <Counter/>
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
